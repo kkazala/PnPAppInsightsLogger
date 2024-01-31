5e8e8a74b770d9c356342a49f2bcf773c62d9bc1
Kinga <22429087+kkazala@users.noreply.github.com>
2024-01-31 15:52:55 +0100

    adding webpack

---
+++ webpack.common.js
@@ -0,0 +1,24 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: { appinsightsloader: './src/index.mts' },
+    module: {
+        rules: [
+            {
+                test: /(\.mts$)|(\.ts?$)/, //test: /\.([cm]?ts|tsx)$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+
+        ],
+    },
+    resolve: {
+        extensions: [".mts", ".ts", ".tsx", ".js"],//['.tsx', '.ts', '.js', '.jsx'],
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+         clean: true,
+    },
+
+};
